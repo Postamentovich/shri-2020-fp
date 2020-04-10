8d3e6d4b7d682514946c33eb229a14b2a1297d07
postamentovich <postamentovich@gmail.com>
2020-04-10 23:31:56 +0300

    выполнил задание №4

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,6 +1,8 @@
+import * as R from "ramda";
+
 /**
  * @file Домашка по FP ч. 1
- * 
+ *
  * Основная задача — написать самому, или найти в FP библиотеках функции anyPass/allPass
  * Эти функции/их аналоги есть и в ramda и в lodash
  *
@@ -13,23 +15,33 @@
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
 
+const SHAPES = { STAR: "star", SQUARE: "square", TRIANGLE: "triangle", CIRCLE: "circle" };
+const COLORS = { RED: "red", ORANGE: "orange", GREEN: "green", BLUE: "blue" };
+const isCircleBlue = R.propEq(SHAPES.CIRCLE, COLORS.BLUE);
+const isStarRed = R.propEq(SHAPES.STAR, COLORS.RED);
+const isSquareOrange = R.propEq(SHAPES.SQUARE, COLORS.ORANGE);
+const isRedEqualsBlue = ({ blue, red }) => blue === red;
+const getColors = R.compose(R.countBy(R.identity), R.values);
+
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
+export const validateFieldN1 = ({ star, square, triangle, circle }) => {
+    if (triangle !== "white" || circle !== "white") {
         return false;
     }
-
-    return star === 'red' && square === 'green';
+    return star === "red" && square === "green";
 };
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = (data) => {
+    console.log(data);
+};
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+
+export const validateFieldN3 = R.compose(isRedEqualsBlue, getColors);
 
 // 4. Синий круг, красная звезда, оранжевый квадрат
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = R.allPass([isCircleBlue, isStarRed, isSquareOrange]);
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
 export const validateFieldN5 = () => false;
